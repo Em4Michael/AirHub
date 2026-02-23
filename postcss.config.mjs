4dcf86d99c6795e002a8cda402b3838627c1d96c
Em4Michael <aniehegc@gmail.com>
2026-02-23 01:11:22 -0500

    vercel delpoy global.css fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+/** @type {import('postcss-load-config').Config} */
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    // Optional – many people also add autoprefixer in v4 projects
+     autoprefixer: {},
+  },
 };
-
-export default config;
